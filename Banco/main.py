b5b308d1d1f41f1cf511cdbada2136cf03719693
arthurlrnl <102841881+arthurlrnl@users.noreply.github.com>
2025-01-11 14:03:13 -0300

    Update main.py

--- Banco/main.py
+++ Banco/main.py
@@ -1,4 +1,4 @@
 if __name__ == "__main__":
-    from BancoGUI import BancoGUI  # Certifique-se de importar corretamente
+    from BancoGUI import BancoGUI 
     app = BancoGUI()
-    app.janela.mainloop()  # Isso inicia a interface
+    app.janela.mainloop()
