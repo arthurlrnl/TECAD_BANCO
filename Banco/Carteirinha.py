be6c64ddca304fb4dbd996ebbdc89c2d815bb0ed
arthurlrnl <102841881+arthurlrnl@users.noreply.github.com>
2025-01-13 21:39:04 -0300

    Update Carteirinha.py

--- Banco/Carteirinha.py
+++ Banco/Carteirinha.py
@@ -1,16 +1,12 @@
 class Carteirinha:
-    """
-    Classe para gerenciar o saldo da carteirinha do RU.
-    Permite adicionar saldo, liberar catraca e consultar histórico.
-    """
-    PRECO_PADRAO = 5.6  # Preço para alunos não FUMPistas
+    PRECO_PADRAO = 5.6
     PRECOS_FUMP = {
-        "I": 0.0,  # Gratuito para FUMPistas Nível I
-        "II": 1.0,  # R$ 1,00 para FUMPistas Nível II
-        "III": 1.0,  # R$ 1,00 para FUMPistas Nível III
-        "IV": 2.0   # R$ 2,00 para FUMPistas Nível IV
+        "I": 0.0,
+        "II": 1.0,
+        "III": 1.0,
+        "IV": 2.0
     }
-    PRECO_PROFESSOR = 13.0  # Preço fixo para professores
+    PRECO_PROFESSOR = 13.0
 
     def __init__(self, tipo_usuario, nivel_fump=None):
         self._saldo_carteirinha = 0.0
@@ -34,19 +30,12 @@ class Carteirinha:
         return self._custo_refeicao
 
     def definir_custo_refeicao(self):
-        """
-        Define o custo da refeição com base no tipo de usuário e nível FUMP.
-        """
         if self._tipo_usuario == "Professor":
             return self.PRECO_PROFESSOR
         elif self._tipo_usuario == "Aluno":
             return self.PRECOS_FUMP.get(self._nivel_fump, self.PRECO_PADRAO)
-        return 20.0  # Valor padrão caso o tipo de usuário não seja reconhecido
-
+        return 20.0 
     def adicionar_saldo(self, valor):
-        """
-        Adiciona saldo à carteirinha.
-        """
         if not isinstance(valor, (int, float)) or valor <= 0:
             raise ValueError("O valor para recarga deve ser um número positivo.")
         self.saldo_carteirinha += valor
@@ -69,15 +58,9 @@ class Carteirinha:
         return self.saldo_carteirinha
 
     def _registrar_historico(self, descricao, valor):
-        """
-        Registra uma movimentação no histórico.
-        """
         self._historico.append({"descricao": descricao, "valor": valor})
 
     def exibir_historico(self):
-        """
-        Retorna o histórico de movimentações da carteirinha como uma string.
-        """
         historico = [
             f"{'Crédito' if mov['valor'] > 0 else 'Débito'}: {mov['descricao']} - R$ {abs(mov['valor']):.2f}"
             for mov in self._historico
